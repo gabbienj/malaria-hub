5c2f5c53ab1f6386c6541d6020fb863ccf51487e
Gabbie Ngwana-Joseph <gabiee@live.co.uk>
2023-09-27 17:21:45 +0100

    Fixed run hmmIBD error and invalid digits error

--- ibd/run_hmmIBD_per_category.R
+++ ibd/run_hmmIBD_per_category.R
@@ -15,13 +15,13 @@ option_list = list(
               help = "Input filename of filtered binary matrix",
               metavar = "character"),
   make_option(c("-m", "--metadata"), type = "character", default = NULL,
-              help = "Full dir to metadata file",
+              help = "Full directory path to metadata file",
               metavar = "character"),
   make_option(c("-c", "--category"), type = "character", default = NULL,
-              help = "Name of country/region",
+              help = "Name of country/region e.g., Indonesia or Southern_SEA",
               metavar = "character"),
   make_option(c("--label_category"), type = "character", default = "country",
-              help = "Label name in metadata for category column",
+              help = "Label name in metadata for category column e.g., country or region",
               metavar = "character"),
   make_option(c("--label_fws"), type = "character", default = "fws",
               help = "Label name in metadata for fws column",
@@ -42,10 +42,10 @@ option_list = list(
               help = "Specify threads number",
               metavar = "numeric"),
   make_option(c("--remove_chr"), type = "character", default = NULL,
-              help = "Chromosomes to remove ex. Pf3D7_API_v3,Pf_M76611",
+              help = "Chromosomes to remove e.g., Pf3D7_API_v3,Pf_M76611",
               metavar = "character"),
   make_option("--regex_chr", type = "character", default = "(.*?)_(.+)_(.*)",
-              help = "Regex pattern for chromosome detection. Default matches Pf3D7_01_v3",
+              help = "Regex pattern for chromosome detection. Default matches Pf3D7_01_v3 or PvP01_01_v1",
               metavar = "character"),
   make_option("--regex_groupid", type = "numeric", default = 3,
               help = "Regex pattern group",
@@ -91,29 +91,29 @@ setDTthreads(threads)
 metadata <- read.csv(met_file, sep = "\t", header = TRUE, stringsAsFactors = FALSE)
 
 if (label_category %in% colnames(metadata)) {
-    av_category <- metadata %>%
-      select(!!sym(label_category)) %>%
-      distinct() %>% pull()
-    message("Available category options: ", paste0(av_category, collapse = ", "))
-    cat("\n")
+  av_category <- metadata %>%
+    select(!!sym(label_category)) %>%
+    distinct() %>% pull()
+  message("Available category options: ", paste0(av_category, collapse = ", "))
+  cat("\n")
 } else {
-    stop("Wrongly specified category. Column name does not exist in metadata file.\n")
+  stop("Wrongly specified category. Column name does not exist in metadata file.\n")
 }
 
 # Load subset of SNP binary matrix for selected category
 if (!is.null(category)) {
-    if (length(category) == 1 & category %in% av_category) {
-        message(category, " found, Processing...\n")
-        metadata <- metadata %>% filter(!!sym(label_category) == category)
-        samples <- c("chr", "pos", "ref", (metadata %>% select(!!sym(label_id)) %>% pull() %>% as.vector()))
-        snp <- fread(bin_mat_file, sep = "\t", select = samples,
-            header = TRUE, data.table = FALSE)
-    } else {
-        stop("Can not find " %+% bgBlue(category) %+% " category. Exitting...\n")
-    }
+  if (length(category) == 1 & category %in% av_category) {
+    message(category, " found, Processing...\n")
+    metadata <- metadata %>% filter(!!sym(label_category) == category)
+    samples <- c("chr", "pos", "ref", (metadata %>% select(!!sym(label_id)) %>% pull() %>% as.vector()))
+    snp <- fread(bin_mat_file, sep = "\t", select = samples,
+                 header = TRUE, data.table = FALSE)
+  } else {
+    stop("Can not find " %+% bgBlue(category) %+% " category. Exiting...\n")
+  }
 } else {
-    message("Category not specified. Processing for all...\n")
-    snp <- fread(bin_mat_file, sep = "\t", header = TRUE, data.table = FALSE)
+  message("Category not specified. Processing for all...\n")
+  snp <- fread(bin_mat_file, sep = "\t", header = TRUE, data.table = FALSE)
 }
 
 # Trim category name
@@ -121,14 +121,14 @@ category_str <- as.character(gsub(" ", "_", category))
 
 # Filter chromosome from matrix
 if (!is.null(rm_chr)) {
-    rm_chr <- strsplit(rm_chr, ",")[[1]]
-    if (any(rm_chr %in% unique(snp$chr))) {
-        snp <- snp %>% filter(!chr %in% rm_chr)
-    } else {
-        stop("Wrong name for chromosomes to remove. Stopping...")
-    }
+  rm_chr <- strsplit(rm_chr, ",")[[1]]
+  if (any(rm_chr %in% unique(snp$chr))) {
+    snp <- snp %>% filter(!chr %in% rm_chr)
+  } else {
+    stop("Wrong name for chromosomes to remove. Stopping...")
+  }
 } else {
-    message("None chromosomes removed. Api and mito should be removed!")
+  message("No chromosomes removed. Apicoplast (API) and mitochondria (MIT) should be removed!")
 }
 
 # Transform chromosome from string to numeric
@@ -143,8 +143,8 @@ if (all(metadata[[label_id]] == colnames(snp[, -c(1:3)]))) {
 # Create input file for hmmIBD
 # Recode missing data
 if (!is.na(na_char)) {
-    snp[snp == na_char] <- NA
-    snp[snp == "."] <- NA
+  snp[snp == na_char] <- NA
+  snp[snp == "."] <- NA
 }
 
 # Reformat matrix to fit hmmIBD format
@@ -162,28 +162,28 @@ rm(snp, snp_hmmibd)
 # Subset reformatted matrices
 snp_hmmibd_02_1 <- snp_hmmibd_1
 write.table(snp_hmmibd_02_1,
-    file.path(workdir, "ibd_matrix_hap_leg.tsv"),
-    quote = FALSE, col.names = TRUE, row.names = FALSE, sep = "\t")
+            file.path(workdir, "ibd_matrix_hap_leg.tsv"),
+            quote = FALSE, col.names = TRUE, row.names = FALSE, sep = "\t")
 
 snp_hmmibd_02_2 <- snp_hmmibd_2
 write.table(snp_hmmibd_02_2,
-    file.path(workdir, sprintf("ibd_matrix_hap_%s.tsv", category_str)),
-    quote = FALSE, col.names = TRUE, row.names = FALSE, sep = "\t")
+            file.path(workdir, sprintf("ibd_matrix_hap_%s.tsv", category_str)),
+            quote = FALSE, col.names = TRUE, row.names = FALSE, sep = "\t")
 
 # Run hmmIBD for population
 message(category)
-  
+
 # Calculate MAF
 maf_sti <- calculate_maf(snp_hmmibd_02_2)
 to_keep_sti <- which(maf_sti >= th_maf)
 
 # Select samples of category with Fws >= threshold_fws
 fws_samples <- metadata %>%
-    filter(!!sym(label_fws) >= threshold_fws) %>%
-    select(!!sym(label_id)) %>%
-    pull() %>%
-    as.vector()
-  
+  filter(!!sym(label_fws) >= threshold_fws) %>%
+  select(!!sym(label_id)) %>%
+  pull() %>%
+  as.vector()
+
 # Select SNPs that passed MAF threshold
 snp_hmmibd_02_2 <- snp_hmmibd_02_2[to_keep_sti, ]
 snp_hmmibd_leg <- snp_hmmibd_02_1[to_keep_sti, ]
@@ -194,15 +194,15 @@ snp_hmmibd_02_2 <- snp_hmmibd_02_2 %>% select(all_of(fws_samples))
 snp_hmmibd_merged <- cbind(snp_hmmibd_leg, snp_hmmibd_02_2)
 
 # Write country matrix to file
-write.table(format(snp_hmmibd_merged, digits = 0),
-    file.path(workdir, sprintf("hmmIBD_%s_maf%s.txt", category_str, as.character(th_maf))),
-    sep = "\t", quote = FALSE, row.names = FALSE)
-  
+write.table(format(snp_hmmibd_merged),
+            file.path(workdir, sprintf("hmmIBD_%s_maf%s.txt", category_str, as.character(th_maf))),
+            sep = "\t", quote = FALSE, row.names = FALSE)
+
 # Run hmmIBD
 string_i <- file.path(workdir,
-    sprintf("hmmIBD_%s_maf%s.txt", category_str, as.character(th_maf)))
+                      sprintf("hmmIBD_%s_maf%s.txt", category_str, as.character(th_maf)))
 string_o <- file.path(workdir,
-    sprintf("hmmIBD_%s_maf%s_out", category_str, as.character(th_maf)))
+                      sprintf("hmmIBD_%s_maf%s_out", category_str, as.character(th_maf)))
 output <- system(command = sprintf("~/software/hmmIBD/hmmIBD -i %s -o %s", string_i, string_o), intern = TRUE)
 write.table(output, file.path(workdir,
-    sprintf("hmmIBD_run_%s.log", category_str)), quote = FALSE, col.names = FALSE, row.names = FALSE)
+                              sprintf("hmmIBD_run_%s.log", category_str)), quote = FALSE, col.names = FALSE, row.names = FALSE)
